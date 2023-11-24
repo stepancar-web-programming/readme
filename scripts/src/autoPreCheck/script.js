1084be794a28a5551f91249ea11225d1516705ae
Denis Suvorov <69597295+Erkobrax@users.noreply.github.com>
2023-11-24 17:46:25 +0300

    add validatePackage

--- scripts/src/autoPreCheck/script.js
+++ scripts/src/autoPreCheck/script.js
@@ -2,6 +2,7 @@ const octonode = require('octonode');
 const fs = require('fs/promises');
 const validatePR = require('.checks/validatePR');
 const validateReadme = require('.checks/validateReadme');
+const validatePackagejson = require('.checks/validatePackagejson');
 
 const OAUTHKEY = process.env.OAUTHKEY;
 const PATHSTUDENTS = 'scripts/src/autoInviteStudents/students.json'; //Предполагается разместить в корне репозитория 'students.json'
@@ -19,31 +20,33 @@ async function validateLab() {
         const repo = org.repo(REPONAME);
 
         const openPullRequests = await repo.prsAsync({ state: 'open' });
-	
+
         const data = await fs.readFile(PATHSTUDENTS, 'utf-8');
         const students = JSON.parse(data);
 
         for (const pr of openPullRequests) {
             const pullRequestNumber = pr.number;
 	    const creatorLogin = pr.user.login;
-	  
+
 	    const isCreatorInStudentList = students.some(student => student.nickname === creatorLogin);
 
 	    if (isCreatorInStudentList) {
 	        // Валидация ПР
 		await validatePR(repo, pullRequestNumber, 'TARGET_BRANCH', 'SOURCE_BRANCH');
-	
+
 		// Валидация README.md
 		await validateReadme(repo, pullRequestNumber);
-	
-		// Другие валидации 
+
+        // Валидация Package.json
+        await validatePackagejson(repo, pullRequestNumber)
+		// Другие валидации
 	    } else {
 		console.error(`Создатель PR не найден в списке студентов.`);
 	    }
 	}
 
     console.log('Все PR проверены.');
-    
+
 
   } catch (error) {
       console.error('Произошла ошибка в validateLab:', error.message);
